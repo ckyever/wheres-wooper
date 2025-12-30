cbe502608ba8029db6e5865f700f5f4bfe87a724
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-30 17:21:32 +1100

    Add styling to heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import "./styles/App.css";
+import styles from "./styles/App.module.css";
 import animatedWooper from "./assets/wooper.gif";
 
 function App() {
   return (
     <>
-      <h1>
-        Where's Wooper?
+      <h1 className={styles.heading}>
         <img src={animatedWooper} alt="Wooper swaying side to side" />
+        Where's Wooper?
       </h1>
     </>
   );
