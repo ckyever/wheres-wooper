13a0963fa376335c003b6d1a7c2479c334bd923a
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-31 16:58:04 +1100

    Add start screen to allow game to load first

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 import Game from "./components/Game.jsx";
+import StartScreen from "./components/StartScreen.jsx";
 
 function App() {
+  const [isGameInProgress, setIsGameInProgress] = useState(false);
+
   return (
     <>
+      <StartScreen
+        isGameLoaded={true}
+        setIsGameInProgress={setIsGameInProgress}
+      />
       <Game />
     </>
   );
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,31 @@
+import styles from "../styles/StartScreen.module.css";
+
+function StartScreen({ isGameLoaded, setIsGameInProgress }) {
+  const startGame = () => {
+    setIsGameInProgress(true);
+    document.getElementById("start-dialog").close();
+  };
+
+  return (
+    <>
+      <dialog id="start-dialog" className={styles["start-dialog"]} open>
+        <div className={styles.container}>
+          <p className={styles.instructions}>
+            This game is Where's Wally, but with Pokemon.
+            <br />
+            Click on the Pokemon that match the 3 outlines.
+            <br />
+            Find them fast to earn a spot on the leaderboard!
+          </p>
+          {isGameLoaded ? (
+            <button onClick={startGame}>Start</button>
+          ) : (
+            <span>Game is loading...</span>
+          )}
+        </div>
+      </dialog>
+    </>
+  );
+}
+
+export default StartScreen;
