dd09679070908b46c20118e856af5b710901de7e
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-31 18:19:26 +1100

    Add initial express app server

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Last updated ${new Date().toISOString()}`);
+});
