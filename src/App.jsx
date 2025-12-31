6582765ff8c7ac0d9448b86888057553b0dbd2f9
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-31 13:25:01 +1100

    Refactor game logic into it's own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,10 @@
-import { compare } from "bcryptjs";
-import { pokemonList, targetPokemon } from "./data.jsx";
-
-import styles from "./styles/App.module.css";
-import animatedWooper from "./assets/wooper.gif";
+import Game from "./components/Game.jsx";
 
 function App() {
-  const handleClick = async (event) => {
-    for (const pokemon of targetPokemon) {
-      if (await compare(event.target.id, pokemon.hash)) {
-        pokemon.isFound = true;
-        const foundTarget = document.getElementById(pokemon.hash);
-        foundTarget.className = "";
-        event.target.classList.add(styles.found);
-        return;
-      }
-    }
-  };
-
   return (
-    <div className={styles.app}>
-      <h1 className={styles.heading}>
-        <img src={animatedWooper} alt="Wooper swaying side to side" />
-        Where's Wooper?
-      </h1>
-      <div className={styles.container}>
-        <div className={styles.targets}>
-          <span>Catch these pokemon</span>
-          <div className={styles.lineup}>
-            {targetPokemon.map((pokemon) => (
-              <img
-                key={pokemon.hash}
-                id={pokemon.hash}
-                src={pokemon.image}
-                className={pokemon.isFound ? "" : styles.missing}
-              />
-            ))}
-          </div>
-        </div>
-        <div className={styles["search-area"]}>
-          {pokemonList.map((pokemon) => {
-            const style = {
-              transform: `rotate(${pokemon.rotation}deg) translateX(${pokemon.xTranslate}px) translateY(${pokemon.yTranslate}px) scale(${pokemon.scale})`,
-            };
-            return (
-              <img
-                key={pokemon.id}
-                id={pokemon.id}
-                src={pokemon.image}
-                className={styles.pokemon}
-                style={style}
-                onClick={(event) => handleClick(event)}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </div>
+    <>
+      <Game />
+    </>
   );
 }
 
