07ceeb603aa7a7693135ccd07fd8e12fd3f01ec1
Ckyever Gaviola <ckyeverg@gmail.com>
2026-01-03 11:16:07 +1100

    Adjust start screen layout

--- client/src/components/StartScreen.jsx
+++ client/src/components/StartScreen.jsx
@@ -18,20 +18,25 @@ function StartScreen({ isGameLoading, sessionId, setIsGameInProgress }) {
     <>
       <dialog id="start-dialog" className={styles["start-dialog"]} open>
         <div className={styles.container}>
-          <Heading />
-          <p className={styles.instructions}>
-            This game is Where's Wally, but with Pokemon.
-            <br />
-            Click on the Pokemon that match the 3 outlines.
-            <br />
-            Find them fast to earn a spot on the leaderboard!
-          </p>
-          {isGameLoading ? (
-            <span>Game is loading...</span>
-          ) : (
-            <button onClick={startGame}>Start</button>
-          )}
-          <Leaderboard />
+          <div className="spacer"></div>
+          <div className={styles["main-content"]}>
+            <Heading />
+            <p className={styles.instructions}>
+              This game is Where's Wally, but with Pokemon.
+              <br />
+              Click on the Pokemon that match the 3 outlines.
+              <br />
+              Find them fast to earn a spot on the leaderboard!
+            </p>
+            {isGameLoading ? (
+              <span>Game is loading...</span>
+            ) : (
+              <button onClick={startGame}>Start</button>
+            )}
+          </div>
+          <div className={styles["right-side"]}>
+            <Leaderboard />
+          </div>
         </div>
       </dialog>
     </>
